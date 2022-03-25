023437eee1fa077b86ebfc0e4201346c995522d2
cdsanchezm <cristiansanchezmalagon19@gmail.com>
2022-03-25 08:54:16 -0500

    conecction

--- models/server.js
+++ models/server.js
@@ -39,7 +39,7 @@ class Server {
     }
 
     routes() {
-        this.app.use( this.usuariosPath, require('../routes/courses'));
+        this.app.use( this.coursePath, require('../routes/courses'));
     }
 
     listen() {
