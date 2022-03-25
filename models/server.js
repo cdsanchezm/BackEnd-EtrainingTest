cc4041b62188911ec7ea0bb7350f54cd0c9cf076
cdsanchezm <cristiansanchezmalagon19@gmail.com>
2022-03-25 08:45:59 -0500

    fixes

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
-        this.usuariosPath = '/api/courses';
+        this.coursePath = '/api/courses';
 
         // Conectar a base de datos
         this.conectarDB();
